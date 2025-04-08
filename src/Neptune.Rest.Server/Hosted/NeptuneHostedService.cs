29fccfeb99654937db276a5e8be455bf889bb3d2
squid <squid@stormwind.it>
2025-04-08 13:32:00 +0200

    feat(NeptuneHostedService.cs): add NeptuneServerConfig dependency injection to NeptuneHostedService constructor feat(NeptuneKeyPairEd25519.cs): implement class for handling Ed25519 key pair generation and operations feat(Neptune.Server.Core.csproj): add NSec.Cryptography package reference and include Cryptography folder feat(Neptune.Tests.csproj): add project reference to Neptune.Server.Core.csproj feat(NeptuneKeyPairEd25519Tests.cs): add tests for NeptuneKeyPairEd25519 class

--- src/Neptune.Rest.Server/Hosted/NeptuneHostedService.cs
+++ src/Neptune.Rest.Server/Hosted/NeptuneHostedService.cs
@@ -1,3 +1,4 @@
+using Neptune.Server.Core.Data.Config;
 using Neptune.Server.Core.Data.Services;
 using Neptune.Server.Core.Interfaces;
 using Neptune.Server.Core.Interfaces.Services;
@@ -12,13 +13,17 @@ public class NeptuneHostedService : IHostedService
 
     private readonly List<ServiceToLoadData> _serviceToLoadData;
 
+    private readonly NeptuneServerConfig _neptuneServerConfig;
+
     public NeptuneHostedService(
-        ILogger<NeptuneHostedService> logger, IServiceProvider serviceProvider, List<ServiceToLoadData> serviceToLoadData
+        ILogger<NeptuneHostedService> logger, IServiceProvider serviceProvider, List<ServiceToLoadData> serviceToLoadData,
+        NeptuneServerConfig neptuneServerConfig
     )
     {
         _logger = logger;
         _serviceProvider = serviceProvider;
         _serviceToLoadData = serviceToLoadData;
+        _neptuneServerConfig = neptuneServerConfig;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
@@ -36,10 +41,17 @@ public class NeptuneHostedService : IHostedService
                 _logger.LogError(ex, "Error during loading service: {Service}", serviceType.ServiceType);
 
                 throw new InvalidOperationException(
-                    $"Error during loading service: {serviceType.ServiceType}", ex
+                    $"Error during loading service: {serviceType.ServiceType}",
+                    ex
                 );
             }
         }
+
+        _logger.LogInformation(
+            "Neptune server started: {NodeName} - {NodeId}",
+            _neptuneServerConfig.NodeName,
+            _neptuneServerConfig.NodeId
+        );
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
